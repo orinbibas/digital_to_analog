b63eb070f423cd1a19186b7659dfe00822b704a6
NoaMatosevich <48092776+NoaMatosevich@users.noreply.github.com>
2019-06-19 11:24:05 +0300

    initial templet

--- digital_to_analog/client.py
+++ digital_to_analog/client.py
@@ -1,3 +1,27 @@
 
 """game interface"""
 
+import zmq
+
+
+def client_socket():
+    context = zmq.Context()
+
+    # socket to talk to server
+    socket = context.socket(zmq.REQ)
+    socket.connect("tcp://localhost:5555")
+
+    n = 10
+
+    # do n requests waiting each time for response
+    for request in range(n):
+        outgoing = b"hello"
+        socket.send(outgoing)
+        incoming = socket.recv()
+
+        print(f"request # {request}: {outgoing} {incoming}")
+    socket.send(b"0")
+
+
+if __name__ == "__main__":
+    client_socket()
