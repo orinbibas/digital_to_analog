a5127e6f111333b817d715ca298facb09d8d59ea
NoaMatosevich <48092776+NoaMatosevich@users.noreply.github.com>
2019-06-20 11:05:25 +0300

    preferences

--- digital_to_analog/digital_to_analog.py
+++ digital_to_analog/digital_to_analog.py
@@ -91,3 +91,23 @@ def append_df_to_log(connection, df):
     with open(filename, 'a') as f:
         df.to_csv(f, header=False, index=False)
 
+#This file contains:
+import os
+#Channel names: leave 'None' if not connected.
+#if connected: enter channel number minus 1 (for 1 enter 0, for 2 enter 1, for 3 enter 2)
+channel1= None
+channel2= None
+channel3= None
+
+#picture names
+
+#game maximal time in seconds
+max_time = 30
+
+#screen properties (optional right now)
+dw = 600
+dh = 476
+
+
+#path to save file
+path_to_save = os.getcwd()
